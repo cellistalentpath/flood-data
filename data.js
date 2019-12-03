7e1a37f42633bde6cfa3bd79f330d17b4a79c7f9
CJ Ellis <cellis@talentpath.com>
2019-12-03 15:53:29 -0600

    stuff

---
+++ data.js
@@ -0,0 +1,35 @@
+var http = require("http");
+
+var hostname = "localhost";
+var port = process.env.PORT || 5000;
+var formattedEverything = {};
+
+var server = http.createServer((request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  if (request.method === "POST" && request.url === "/map/formatted") {
+    request.on("data", function(chunk) {
+      for (id in formattedEverything) {
+        if (
+          formattedEverything[id].formattedHeld ===
+          JSON.parse(chunk.toString()).formattedHeld
+        ) {
+          formattedEverything[id].totalResidents = JSON.parse(
+            chunk.toString()
+          ).totalResidents;
+        }
+      }
+      formattedEverything[JSON.parse(chunk.toString()).id] = JSON.parse(
+        chunk.toString()
+      );
+      response.end();
+    });
+  }
+  if (request.method === "GET" && request.url === "/map/formatted") {
+    response.write(JSON.stringify(formattedEverything));
+    response.end();
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
